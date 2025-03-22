a7192d4d069de8b40ee7efaffcf5c4beba6c2c5a
Gerakl1123 <germanniyazgg@gmail.com>
2025-03-22 20:43:32 +0300

    пример кода для использования мини библеотеки

---
+++ test.cpp
@@ -0,0 +1,19 @@
+﻿#include "Rotate.h"
+
+int main() {
+    int rows = 3, cols = 2;
+    Rotate rv(rows, cols, 100);
+
+    cout << "Original matrix:\n";
+    rv.PrintDisplay();
+
+    rv.Rotate90();
+    cout << "Matrix after 90 degree rotation:\n";
+    rv.PrintDisplay();
+
+    rv.Rotate180();
+    cout << "Matrix after 180 degree rotation:\n";
+    rv.PrintDisplay();
+
+    return 0;
+}
